a7d4c27b554bb9901ba3b3b109d34dd56aa9ae58
gomazarashi <Seto42357@gmail.com>
2024-07-04 15:40:52 +0900

    if文について学習

---
+++ branches/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    // numberの値は、{}です
+    println!("The value of number is: {}", number);
+    // conditionがtrueなので5が代入されている
+}
